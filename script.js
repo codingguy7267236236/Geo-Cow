5a28182c0d0658d5002cd60b0af922940d529535
Michael <158330984+codingguy7267236236@users.noreply.github.com>
2024-02-01 19:39:48 +0000

    Initial Files
    Added all the files

---
+++ script.js
@@ -0,0 +1,44 @@
+window.addEventListener('load', function(){
+  var touchsurface = document.getElementById('touchsurface'),
+    startX,
+    startY,
+    dist,
+    threshold = 150, //required min distance traveled to be considered swipe
+    allowedTime = 200, // maximum time allowed to travel that distance
+    elapsedTime,
+    startTime
+ 
+  function handleswipe(isrightswipe){
+    let sd = document.getElementById("side");
+    if (isrightswipe)
+      sd.setAttribute("class","side");
+    else{
+      sd.setAttribute("class","disable");
+    }
+  }
+ 
+  touchsurface.addEventListener('touchstart', function(e){
+    touchsurface.innerHTML = ''
+    var touchobj = e.changedTouches[0]
+    dist = 0
+    startX = touchobj.pageX
+    startY = touchobj.pageY
+    startTime = new Date().getTime() // record time when finger first makes contact with surface
+    e.preventDefault()
+  }, false)
+ 
+  touchsurface.addEventListener('touchmove', function(e){
+    e.preventDefault() // prevent scrolling when inside DIV
+  }, false)
+ 
+  touchsurface.addEventListener('touchend', function(e){
+    var touchobj = e.changedTouches[0]
+    dist = touchobj.pageX - startX // get total dist traveled by finger while in contact with surface
+    elapsedTime = new Date().getTime() - startTime // get time elapsed
+    // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
+    var swiperightBol = (elapsedTime <= allowedTime && dist >= threshold && Math.abs(touchobj.pageY - startY) <= 100)
+    handleswipe(swiperightBol)
+    e.preventDefault()
+  }, false)
+ 
+}, false) // end window.onload
